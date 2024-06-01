053c0179cf943eafacc55c0fc0ef95c9a715fc6c
Abner Batista <abnerjob16@gmail.com>
2024-06-01 07:17:22 -0300

    layout concluido

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     const dropdown = $("#dropdown");
 
-    $(".langList").on('mouseenter', function(){
+    $(".langList").on('mouseenter', function () {
         dropdown.show();
     });
 
-    $(".langList").on('mouseleave', function(){
+    $(".langList").on('mouseleave', function () {
         dropdown.hide();
     });
-});
+
+    /* APLICAR A PARTIR DE CERTA RESOLUÇÃO (MOBILE)
+    
+    $('nav div a').click(function () {
+        $(this).css("color", "#979797").css("transition", "color 0.15s");
+
+        const thiis = $('nav div a');
+
+        setTimeout(function(){
+            thiis.css("color", "black")
+        }, 300)
+})
+    
+    */
+
+})
